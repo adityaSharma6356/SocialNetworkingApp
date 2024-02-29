454cf7d56b11e049b51e82bd38119db2d25e1f26
adityaSharma6356 <115709232+adityaSharma6356@users.noreply.github.com>
2024-02-29 20:41:26 +0530

    working Now!!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("kotlin-android")
-    kotlin("kapt")
-    id ("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
-    id ("kotlin-parcelize")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 
 }
 
@@ -46,7 +43,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.9"
     }
     packaging {
         resources {
@@ -56,42 +53,33 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation ("androidx.compose.ui:ui-test-manifest")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
 
     implementation("androidx.compose.material:material-icons-extended:1.6.2")
 
-    // Dagger - Hilt
-   // implementation("com.google.dagger:hilt-android:2.49")
-
-    //kapt ("com.google.dagger:hilt-android-compiler:2.37")
-
-   // kapt("androidx.hilt:hilt-compiler:1.2.0")
-
     implementation("com.google.dagger:hilt-android:2.50")
-    //kapt("com.google.dagger:hilt-android-compiler:2.44")
+    ksp("com.google.dagger:hilt-compiler:2.50")
     implementation("androidx.hilt:hilt-work:1.2.0")
     //kapt("androidx.hilt:hilt-compiler:1.2.0")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-
-    annotationProcessor ("com.google.dagger:hilt-compiler:2.50")
-
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,13 @@
 
 buildscript {
     dependencies{
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
     }
 }
 
 plugins {
     id("com.android.application") version "8.2.2" apply false
-    id ("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 
 }
